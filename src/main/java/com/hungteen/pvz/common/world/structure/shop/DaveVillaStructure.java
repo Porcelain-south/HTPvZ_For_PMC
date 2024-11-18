4b743dfdfd65472047c3fab3944f5e070e7ab3b7
Porcelain-south <south-qing@qq.com>
2024-11-18 11:38:22 +0800

    严苛性生成调整

--- src/main/java/com/hungteen/pvz/common/world/structure/shop/DaveVillaStructure.java
+++ src/main/java/com/hungteen/pvz/common/world/structure/shop/DaveVillaStructure.java
@@ -46,48 +46,56 @@ public class DaveVillaStructure extends PVZStructureBase<NoFeatureConfig> {
 	        int z = (chunkZ << 4);
 	        int dx = 0, dz = 0;
 			switch(rotation) {
-			case CLOCKWISE_90:{
-				dx -= 2 * len;
-				dz += len;
-				break;
+				case CLOCKWISE_90:{
+					dx -= 2 * len;
+					dz += len;
+					break;
+				}
+				case CLOCKWISE_180:{
+					dx -= len;
+					dz -= 2 * len;
+					break;
+				}
+				case COUNTERCLOCKWISE_90:{
+					dx += 2 * len;
+					dz -= len;
+					break;
+				}
+				default:{
+					dx += len;
+					dz += 2 * len;
+					break;
+				}
 			}
-			case CLOCKWISE_180:{
-				dx -= len;
-				dz -= 2 * len;
-				break;
-			}
-			case COUNTERCLOCKWISE_90:{
-				dx += 2 * len;
-				dz -= len;
-				break;
-			}
-			default:{
-				dx += len;
-				dz += 2 * len;
-				break;
-			}
-			}
-	        int h1 = generator.getFirstOccupiedHeight(x, z, Heightmap.Type.WORLD_SURFACE_WG);
-			boolean isPlains1 = generator.getBiomeSource().getNoiseBiome(chunkX,h1,chunkZ).getBiomeCategory() == Biome.Category.PLAINS;
 
-	        int h2 = generator.getFirstOccupiedHeight(x, z + dz, Heightmap.Type.WORLD_SURFACE_WG);
-			boolean isPlains2 = generator.getBiomeSource().getNoiseBiome(chunkX,h2,chunkZ + dz/16).getBiomeCategory() == Biome.Category.PLAINS;
+			int[] h = new int[9];
+			for(int i = 0; i < 3; i++)
+			{
+				for(int j = 0; j < 3; j++)
+				{
+					h[3*i + j] = generator.getFirstOccupiedHeight(x + i*dx/2, z + j*dz/2, Heightmap.Type.WORLD_SURFACE_WG);
+				}
+			}
+			int avgh = 0;
+			int maxh = h[0];
+			for(int i = 0; i < 9; i++)
+			{
+				avgh += h[i];
+				maxh = Math.max(maxh, h[i]);
+			}
+			avgh /= 9;
 
-	        int h3 = generator.getFirstOccupiedHeight(x + dx, z, Heightmap.Type.WORLD_SURFACE_WG);
-			boolean isPlains3 = generator.getBiomeSource().getNoiseBiome(chunkX + dx/16,h3,chunkZ).getBiomeCategory() == Biome.Category.PLAINS;
 
-	        int h4 = generator.getFirstOccupiedHeight(x + dx, z + dz, Heightmap.Type.WORLD_SURFACE_WG);
-			boolean isPlains4 = generator.getBiomeSource().getNoiseBiome(chunkX + dx/16,h4,chunkZ + dz/16).getBiomeCategory() == Biome.Category.PLAINS;
-
-	        int h = (h1 + h2 + h3 + h4) / 4;
 			if(ConfigUtil.StrictVilla())
 			{
-				if (!isPlains1 || !isPlains2 || !isPlains3 || !isPlains4)
-					return;
-				if (Math.abs(h - h1) > 5 ||Math.abs(h - h2) > 5 ||Math.abs(h - h2) > 5 ||Math.abs(h - h4) > 5)
-					return;
+				for(int i = 0; i < 9; i++)
+				{
+					if(Math.abs(h[i] - avgh) > 4)
+						return;
+				}
+				avgh = Math.min(avgh + 2, maxh);
 			}
-			BlockPos blockpos = new BlockPos(x, h, z);
+			BlockPos blockpos = new BlockPos(x, avgh, z);
 			DaveVillaComponents.generate(templateManagerIn, blockpos, rotation, this.pieces, this.random);
 			this.calculateBoundingBox();
 		}
